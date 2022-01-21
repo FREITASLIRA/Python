14bedbf4e100cf78d80c210b4553b2c7ab288318
Freitas Lira <li_raf@hotmail.com>
2022-01-20 21:47:58 -0300

    Exercício menor número

---
+++ pythonProject1/Menor numero.py
@@ -0,0 +1,13 @@
+
+primeiro = int(input("Qual o primeiro número: "))
+segundo = int(input("Qual segundo número: "))
+terceiro = int(input("Qual terceiro número: "))
+
+if primeiro < segundo and primeiro < terceiro:
+    menor = primeiro
+elif segundo < terceiro: #elif faz um encadeamento para nao ficar repitido o if(SE)
+    menor = segundo
+else:
+    menor = terceiro
+
+print(f"O menor valor é: {menor}")
